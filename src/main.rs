de423d463a8e7ede5aeaeccd1bab8d80fe05b049
Guillaume Bogard <g.bogard@qwant.com>
2024-05-22 10:56:04 +0200

    Sea ORM

--- src/main.rs
+++ src/main.rs
@@ -13,12 +13,10 @@ async fn main() {
     // initialize tracing
     tracing_subscriber::fmt::init();
 
-    // build our application with a route
     let app = Router::new()
         .nest("/", routes::public_site_router())
         .with_state(Arc::new(AppState::new()));
 
-    // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     info!("Server is listening on port 3000 (http://localhost:3000)");
     axum::serve(listener, app).await.unwrap();
