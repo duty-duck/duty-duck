a13d88b215ee53525efce6005583309b15a6ce10
Guillaume Bogard <hey@guillaumebogard.dev>
2024-10-03 14:43:24 +0000

    Improve end to end tests and make them runnable on the preprod env

--- frontend/test/e2e/signup.spec.ts
+++ frontend/test/e2e/signup.spec.ts
@@ -1,16 +1,9 @@
 import { describe, test } from 'vitest'
-import { setup } from './';
+import { config, fakeUser, setup } from './';
 import { createPage } from '@nuxt/test-utils';
-import { expect, type Page } from '@playwright/test';
+import { expect } from '@playwright/test';
 import { rand } from '@vueuse/core';
 
-export const fakeUser = {
-    firstname: "Jane",
-    lastName: "Doe",
-    emailAddress: "jane@e2etests.com",
-    password: "5pSQox3rLDxV@3AMx",
-    orgName: "Tests Inc."
-}
 
 describe('Signup spec', async () => {
     await setup();
@@ -33,7 +26,10 @@ async function signUp(user: typeof fakeUser) {
     await page.getByRole('textbox', { name: 'Password', exact: true }).fill(user.password);
     await page.getByRole('textbox', { name: 'Password confirmation', exact: true }).fill(user.password);
     await page.getByRole('textbox', { name: 'Your organization' }).fill(user.orgName);
+    await expect(page.getByRole('button', { name: 'Sign up' })).toBeEnabled();
     await page.getByRole('button', { name: 'Sign up' }).click();
+
+    await page.waitForResponse(new RegExp("signup"))
 
     // Wait for comfirmation
     await expect(page.getByRole('heading', { name: 'Thank you for registering' })).toBeVisible();
@@ -49,13 +45,13 @@ async function signUp(user: typeof fakeUser) {
     await expect(page.getByRole("heading", { name: "Email verification" })).toBeVisible();
 
     // Go to the mailbox
-    await page.goto("http://localhost:1080");
+    await page.goto(config.mailbox);
     await page.getByRole('link', { name: 'Verify email' }).first().click();
     await page.frameLocator('.panel-html').getByRole('link', { name: 'Link to e-mail address verification' }).click();
 
     await new Promise(resolve => setTimeout(resolve, 2000));
     // Assert that the new user is signed in 
-    await page.goto('http://localhost:5173/en/dashboard');
+    await page.goto(config.host + '/en/dashboard');
     await expect(page.locator('#auth-menu a.dropdown-toggle')).toBeVisible();
     await expect(page.locator('#auth-menu a.dropdown-toggle')).toHaveText(`${user.firstname} ${user.lastName}`);
 }
