6ae5d8e236684136ccf6a0d8c12991693fc51cb8
Guillaume Bogard <g.bogard@qwant.com>
2024-12-20 10:31:36 +0000

    Improved dashboard home

--- components/frontend/pages/dashboard/index.vue
+++ components/frontend/pages/dashboard/index.vue
@@ -9,63 +9,81 @@ const { refresh: refreshDownMonitorsCount, data: downMonitorsCount } = await htt
 </script>
 <template>
   <BContainer>
-    <h2>{{ $t('dashboard.home.pageTitle') }}</h2>
+    <!-- Greeting -->
+    <section class="my-5">
+      <h1 class="display-4">{{ $t('dashboard.home.greeting', { name: auth.userProfile?.user.firstName }) }}</h1>
+      <p class="lead text-secondary">{{ $t('dashboard.home.greetingDescription') }}</p>
+    </section>
 
-    <!-- Phone number verification -->
-    <BAlert variant="info" class="mb-3"
-      :model-value="auth.userProfile && (!auth.userProfile.user.phoneNumber || !auth.userProfile.user.phoneNumberVerified)">
-      <h5>{{ $t('dashboard.home.phoneNumberVerificationRequired') }}</h5>
-      <p>
-        {{ $t('dashboard.home.phoneNumberVerificationRequiredDescription') }}
-      </p>
-      <BButton :to="localePath('/dashboard/myAccount')" variant="outline-info" class="icon-link">
-        <Icon name="ph:phone-call-fill" />
-        {{ $t('dashboard.home.verifyPhoneNumber') }}
-      </BButton>
-    </BAlert>
 
-    <!-- Push notifications -->
-    <BAlert variant="info" class="mb-push-notifications-alert" :model-value="!thisDevice">
-      <h5>{{ $t('dashboard.home.pushNotificationsRequired') }}</h5>
-      <p>
-        {{ $t('dashboard.home.pushNotificationsRequiredDescription') }}
-      </p>
-      <BButton :to="localePath('/dashboard/myAccount')" variant="outline-info" class="icon-link">
-        <Icon name="ph:bell-duotone" />
-        {{ $t('dashboard.home.configurePushNotifications') }}
-      </BButton>
-    </BAlert>
-
-    <h3>{{ $t('dashboard.home.overview') }}</h3>
-    <div class="row row-gap-2">
-      <div class="col-sm-6 col-md-3">
-        <BCard class="text-center">
-          <p class="h1 d-flex align-items-center justify-content-center">
-            <Icon name="ph:warning-circle-duotone" v-if="incidentCount" />
-            <Icon name="ph:check-circle-duotone" v-else />
-            {{ incidentCount }}
-          </p>
-          <h4 class="h6">{{ $t('dashboard.home.ongoingIncidents', incidentCount || 0) }}</h4>
-          <NuxtLink to="/dashboard/incidents" class="icon-link">
-            {{ $t('dashboard.home.goToIncidents') }}
-            <Icon name="ph:arrow-right" />
-          </NuxtLink>
-        </BCard>
+    <!-- Overview section -->
+    <section class="mb-5">
+      <h3 class="mb-3">{{ $t('dashboard.home.overview') }}</h3>
+      <div class="row row-gap-2">
+        <div class="col-sm-6 col-md-3">
+          <BCard class="text-center">
+            <p class="h1 d-flex align-items-center justify-content-center">
+              <Icon name="ph:warning-circle-duotone" v-if="incidentCount" />
+              <Icon name="ph:check-circle-duotone" v-else />
+              {{ incidentCount }}
+            </p>
+            <h4 class="h6">{{ $t('dashboard.home.ongoingIncidents', incidentCount || 0) }}</h4>
+            <NuxtLink to="/dashboard/incidents" class="icon-link">
+              {{ $t('dashboard.home.goToIncidents') }}
+              <Icon name="ph:arrow-right" />
+            </NuxtLink>
+          </BCard>
+        </div>
+        <div class="col-sm-6 col-md-3">
+          <BCard class="text-center">
+            <p class="h1 d-flex align-items-center justify-content-center">
+              <Icon name="ph:warning-circle-duotone" v-if="downMonitorsCount" />
+              <Icon name="ph:check-circle-duotone" v-else />
+              {{ downMonitorsCount }}
+            </p>
+            <h4 class="h6">{{ $t('dashboard.home.downMonitors', downMonitorsCount || 0) }}</h4>
+            <NuxtLink to="/dashboard/httpMonitors" class="icon-link">
+              {{ $t('dashboard.home.goToMonitors') }}
+              <Icon name="ph:arrow-right" />
+            </NuxtLink>
+          </BCard>
+        </div>
       </div>
-      <div class="col-sm-6 col-md-3">
-        <BCard class="text-center">
-          <p class="h1 d-flex align-items-center justify-content-center">
-            <Icon name="ph:warning-circle-duotone" v-if="downMonitorsCount" />
-            <Icon name="ph:check-circle-duotone" v-else />
-            {{ downMonitorsCount }}
-          </p>
-          <h4 class="h6">{{ $t('dashboard.home.downMonitors', downMonitorsCount || 0) }}</h4>
-          <NuxtLink to="/dashboard/httpMonitors" class="icon-link">
-            {{ $t('dashboard.home.goToMonitors') }}
-            <Icon name="ph:arrow-right" />
-          </NuxtLink>
-        </BCard>
+    </section>
+
+    <!-- Suggested actions-->
+    <section class="mb-4">
+      <h3 class="mb-4 fs-4">{{ $t('dashboard.home.suggestedActions') }}</h3>
+      <div class="row">
+        <div class="col">
+          <!-- Phone number verification -->
+          <BAlert variant="info" class="mb-3"
+            :model-value="auth.userProfile && (!auth.userProfile.user.phoneNumber || !auth.userProfile.user.phoneNumberVerified)">
+            <h5>{{ $t('dashboard.home.phoneNumberVerificationRequired') }}</h5>
+            <p>
+              {{ $t('dashboard.home.phoneNumberVerificationRequiredDescription') }}
+            </p>
+            <BButton :to="localePath('/dashboard/myAccount')" variant="outline-info" class="icon-link">
+              <Icon name="ph:phone-call-fill" />
+              {{ $t('dashboard.home.verifyPhoneNumber') }}
+            </BButton>
+          </BAlert>
+        </div>
+
+        <div class="col">
+          <!-- Push notifications -->
+          <BAlert variant="info" class="mb-push-notifications-alert" :model-value="!thisDevice">
+            <h5>{{ $t('dashboard.home.pushNotificationsRequired') }}</h5>
+            <p>
+              {{ $t('dashboard.home.pushNotificationsRequiredDescription') }}
+            </p>
+            <BButton :to="localePath('/dashboard/myAccount')" variant="outline-info" class="icon-link">
+              <Icon name="ph:bell-duotone" />
+              {{ $t('dashboard.home.configurePushNotifications') }}
+            </BButton>
+          </BAlert>
+        </div>
       </div>
-    </div>
+    </section>
   </BContainer>
 </template>
